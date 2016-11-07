a5eaa1511f6fc93ff8b22701ecaa21dabc403fe2
Guenter Schafranek <gschafra@web.de>
2016-11-08 00:12:01 +0100

    Artist component
    Implemented artist component and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,19 @@ import { AppComponent } from './app.component';
 import { AboutComponent } from './components/about/about.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SearchComponent } from './components/search/search.component';
+import { ArtistComponent } from './components/artist/artist.component';
 
 import { SpotifyService } from './services/spotify.service';
 
 import {AppRouting} from './app.routing';
+
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
     NavbarComponent,
-    SearchComponent
+    SearchComponent,
+    ArtistComponent
   ],
   imports: [
     BrowserModule,
